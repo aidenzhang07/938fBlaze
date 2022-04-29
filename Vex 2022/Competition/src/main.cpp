8842ceedd26a30ab6857ca54d37a4793ea661cc5
Aiden Zhang <aidenzhang@live.com>
2022-04-29 16:34:01 -0700

    Initial Upload for Competition Code

---
+++ Vex 2022/Competition/src/main.cpp
@@ -0,0 +1,241 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       VEX                                                       */
+/*    Created:      Thu Sep 26 2019                                           */
+/*    Description:  Competition Template                                      */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// Controller1          controller                    
+// FR                   motor         2               
+// FL                   motor         3               
+// BR                   motor         4               
+// BL                   motor         21              
+// BLift                motor         14              
+// Conveyer             motor         16              
+// ML                   motor_group   6, 7            
+// LimitSwitchC         limit         C               
+// LatchPneumatic       digital_out   B               
+// LiftPneumantic       digital_out   D               
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+#include "autonFunctionHeader.h"
+#include "driveFunctionHeader.h"
+
+using namespace vex;
+
+// A global instance of competition
+competition Competition;
+// define your global instances of motors and other devices here
+inertial imu = inertial(vex::PORT17);
+motor_group driveTrainLeft = motor_group(FL, BL);
+motor_group driveTrainRight = motor_group(FR, BR);
+smartdrive driveTrain = smartdrive(driveTrainLeft, driveTrainRight, imu, 12.5663706143591, 1, 1, distanceUnits::in, 1.0);
+
+
+/*---------------------------------------------------------------------------*/
+/*                          Pre-Autonomous Functions                         */
+/*                                                                           */
+/*  You may want to perform some actions before the competition starts.      */
+/*  Do them in the following function.  You must return from this function   */
+/*  or the autonomous and usercontrol tasks will not be started.  This       */
+/*  function is only called once after the V5 has been powered on and        */
+/*  not every time that the robot is disabled.                               */
+/*---------------------------------------------------------------------------*/
+
+void pre_auton(void) {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+
+  // All activities that occur before the competition starts
+  // Example: clearing encoders, setting servo positions, ...
+}
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                              Autonomous Task                              */
+/*                                                                           */
+/*  This task is used to control your robot during the autonomous phase of   */
+/*  a VEX Competition.                                                       */
+/*                                                                           */
+/*  You must modify the code to add your own robot specific commands here.   */
+/*---------------------------------------------------------------------------*/
+
+void autonomous(void) 
+{
+
+  // ..........................................................................
+  // Insert autonomous user code here.
+  // ..........................................................................
+  
+  autonLatchOpen(false);
+
+  smoothDrive(25, 100);
+  
+  driveTrain.turnToRotation(-43, deg, 100, velocityUnits::pct, true);
+  
+  smoothDrive(46, 100);
+
+  autonLatchClose(true);
+
+  ML.spinFor(150, vex::rotationUnits (deg), 100, vex::velocityUnits (pct), true);
+
+  smoothDrive(-40, 100);
+
+  BLift.spinFor(-1000, vex::rotationUnits (deg), 100, vex::velocityUnits (pct), true);
+
+  driveTrain.turnToRotation(-85 , deg, 100, velocityUnits::pct, true);
+
+  smoothDrive (-24, 100);
+
+  BLift.spinFor(650, vex::rotationUnits (deg), 100, vex::velocityUnits (pct), true);
+}
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                              User Control Task                            */
+/*                                                                           */
+/*  This task is used to control your robot during the user control phase of */
+/*  a VEX Competition.                                                       */
+/*                                                                           */
+/*  You must modify the code to add your own robot specific commands here.   */
+/*---------------------------------------------------------------------------*/
+
+void usercontrol(void) {
+  // User control code here, inside the loop
+  Conveyer.setVelocity(100, percent);
+  bool HToggling = false;
+  bool DirToggling = false;
+  bool HToggle = false;
+  bool DirToggle = true;
+  bool ConveyerToggle = false;
+  bool ConveyerToggling = false;
+  bool UpToggle = false;
+  bool DownToggle = false;
+  bool LatchToggle = true;
+  bool LatchToggling = false;
+  bool LiftToggle = false;
+  bool LiftToggling = false;
+  float Y;
+  float Z;
+  ML.setVelocity(100, percent);
+  BLift.setVelocity(100, percent);
+  Conveyer.setVelocity(100, percent);
+
+  Controller1.Screen.clearScreen();
+  Controller1.Screen.setCursor(0, 0);
+  Controller1.Screen.print("Emergency Hold: off");
+  Controller1.Screen.setCursor(2, 0);
+  Controller1.Screen.print("RingLift Dir: Forward");
+  bool HoldToggleValue = false;
+  bool DirToggleValue = false;
+  while (1) {
+    Y = Controller1.Axis1.position(pct);
+    Z = Controller1.Axis3.position(pct);
+
+    HToggleDrive(HToggle, HToggling); //Hold toggle
+    DirToggleDrive(DirToggle, DirToggling, ConveyerToggle, UpToggle, DownToggle); //Latch toggle 
+    RingLift (DirToggle, ConveyerToggle);
+    Screen_Write(HToggle, DirToggle, DirToggleValue, HoldToggleValue); //Updates the display.
+    DriveAlgorithm(Y, Z, driveTrainLeft, driveTrainRight); //Drive Code.
+    MLLift(); //Main Lift
+     //Front Lift Latch
+    BLiftDrive(); //Back Lift
+    Latch(LatchToggle, LatchToggling);
+    LiftMove (LiftToggle, LiftToggling);
+    
+    
+
+    /*
+    if (Controller1.ButtonDown.pressing() && !HToggling) {
+      HToggling = true;
+      HToggle = !HToggle; 
+      Screen_Write();
+    }
+
+    else if (!Controller1.ButtonDown.pressing())
+    {
+      HToggling = false;
+    }
+
+    if (HToggle == true)
+    {
+      FR.setStopping(hold);
+      FL.setStopping(hold);
+      BR.setStopping(hold);
+      BL.setStopping(hold);
+    }
+    
+    else if (HToggle == false)
+    {
+      FR.setStopping(coast);
+      FL.setStopping(coast);
+      BL.setStopping(coast);
+      BR.setStopping(coast);
+    }
+
+    if (Controller1.ButtonUp.pressing() && DirToggling == false)
+    {
+      DirToggle = !DirToggle;
+      DirToggling = true;
+      Screen_Write();
+    }
+
+    else if (!Controller1.ButtonUp.pressing())
+    {
+      DirToggling = false;
+    }
+
+    if (Controller1.ButtonL1.pressing()) {
+      ML.spin(forward);
+    } else if (Controller1.ButtonL2.pressing()) {
+      ML.spin(reverse);
+    } else {
+      ML.stop(hold);
+    }
+
+    if (Controller1.ButtonR1.pressing() && !LimitSwitchB) {
+      Conveyer.spin(forward);
+    } else if (Controller1.ButtonR2.pressing()) {
+      Conveyer.spin(reverse);
+    } 
+    else if (DirToggle)
+    {
+      Conveyer.stop(hold);
+    }
+    else
+    {
+      Conveyer.stop(coast);
+    }
+
+    if (Controller1.ButtonX.pressing()) {
+      BLift.spin(forward);
+    } else if (Controller1.ButtonB.pressing()) {
+      BLift.spin(reverse);
+    } else {
+      BLift.stop(hold);
+    } */
+  }
+}
+
+//
+// Main will set up the competition functions and callbacks.
+//
+int main() {
+  // Set up callbacks for autonomous and driver control periods.
+  Competition.autonomous(autonomous);
+  Competition.drivercontrol(usercontrol);
+
+  // Run the pre-autonomous function.
+  pre_auton();
+
+  // Prevent main from exiting with an infinite loop.
+  while (true) {
+    wait(100, msec);
+  }
+}
---
+++ Vex 2022/Competition/src/robot-config.cpp
@@ -0,0 +1,36 @@
+#include "vex.h"
+
+using namespace vex;
+using signature = vision::signature;
+using code = vision::code;
+
+// A global instance of brain used for printing to the V5 Brain screen
+brain  Brain;
+
+// VEXcode device constructors
+controller Controller1 = controller(primary);
+motor FR = motor(PORT2, ratio18_1, true);
+motor FL = motor(PORT3, ratio18_1, false);
+motor BR = motor(PORT4, ratio18_1, true);
+motor BL = motor(PORT21, ratio18_1, false);
+motor BLift = motor(PORT14, ratio18_1, false);
+motor Conveyer = motor(PORT16, ratio18_1, true);
+motor MLMotorA = motor(PORT6, ratio36_1, false);
+motor MLMotorB = motor(PORT7, ratio36_1, true);
+motor_group ML = motor_group(MLMotorA, MLMotorB);
+limit LimitSwitchC = limit(Brain.ThreeWirePort.C);
+digital_out LatchPneumatic = digital_out(Brain.ThreeWirePort.B);
+digital_out LiftPneumantic = digital_out(Brain.ThreeWirePort.D);
+
+// VEXcode generated functions
+// define variable for remote controller enable/disable
+bool RemoteControlCodeEnabled = true;
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void vexcodeInit( void ) {
+  // nothing to initialize
+}
