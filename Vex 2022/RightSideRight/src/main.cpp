62ec4a30e7efe83d304fca63924a462942749d0f
Aiden Zhang <aidenzhang@live.com>
2022-04-30 13:30:47 -0700

    Somewhat working right side right goal auton code

--- Vex 2022/RightSideRight/src/main.cpp
+++ Vex 2022/RightSideRight/src/main.cpp
@@ -72,39 +72,37 @@ void autonomous(void)
   // ..........................................................................
   // Insert autonomous user code here.
   // ..........................................................................
-  BLift.setPosition(0, vex::rotationUnits (deg));
 
+  ML.setPosition(0, vex::rotationUnits (deg));
+
+  BL.setPosition(0, vex::rotationUnits (deg));
   driveTrain.setRotation(0, vex::rotationUnits (deg));
 
-  autonLatchOpen(false);
+  autonLatchOpen (false);
 
-  smoothDrive(25, 100);
-  
-  driveTrain.turnToRotation(-45.25, deg, 100, velocityUnits::pct, true);
-  
-  smoothDrive(46, 100);
- 
-  autonLatchClose(true);
+  smoothDrive (54, 100);
 
-  ML.setStopping(hold);
+  autonLatchClose (false);
 
   ML.spinToPosition(100, vex::rotationUnits (deg), 100, vex::velocityUnits (pct), true);
 
-  smoothDrive(-43, 100);
+  smoothDrive (-6, 100);
 
-  BLift.spinToPosition(-985, vex::rotationUnits (deg), 100, vex::velocityUnits (pct), true);
+  BLift.spinToPosition(-990, vex::rotationUnits (deg), 100, vex::velocityUnits (pct), true);
 
-  driveTrain.turnToRotation(-88 , deg, 100, velocityUnits::pct, true);
+  driveTrainLeft.spinFor (-500, vex::rotationUnits (deg), 100, vex::velocityUnits (pct), true);
 
-  smoothDrive (-15, 100);
+  smoothDrive (-12, 100);
 
-  BLift.spinToPosition(-485, vex::rotationUnits (deg), 100, vex::velocityUnits (pct), true);
+  BLift.spinToPosition(-500, vex::rotationUnits (deg), 100, vex::velocityUnits (pct), true);
 
-  ML.spinToPosition(200, vex::rotationUnits (deg), 100, vex::velocityUnits (pct), true);
+  ML.spinToPosition(200,vex::rotationUnits (deg), 100, vex::velocityUnits (pct), true);
 
   autonRingLiftOn (75, true);
 
-  wait (1500, msec);
+  wait (1000, msec);
+
+  driveTrain.turnToRotation (85, vex::rotationUnits (deg), 50, vex::velocityUnits (pct), true);
 
   autonRingLiftOff ();
 }
